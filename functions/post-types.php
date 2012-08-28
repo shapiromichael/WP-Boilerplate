6b2c1c0dead21f117618d0403a9ac55555780c05
Elisha Shapiro <elishas@sinapsa.co.il>
2012-08-28 14:22:12 +0300

    Changed indentation in post-types.php

--- functions/post-types.php
+++ functions/post-types.php
@@ -12,34 +12,34 @@
 	function theme_register_posttype_example(){
 		
 		$labels = array(
-			'name' 			 		=> 'Example',
-			'singular_name' 		=> 'Example',
-			'add_new' 		 		=> 'Add New Example',
-			'add_new_item'  		=> 'Add New Example',
-			'edit_item' 	 		=> 'Edit Example',
-			'new_item'		 		=> 'New Example',
-			'all_items' 	 		=> 'All Examples',
-			'view_item' 	 		=> 'View Examples',
-			'search_items'  		=> 'Search Examples',
-			'not_found' 	 		=>  'No Examples Found',
+			'name'               => 'Example',
+			'singular_name'      => 'Example',
+			'add_new'            => 'Add New Example',
+			'add_new_item'       => 'Add New Example',
+			'edit_item'          => 'Edit Example',
+			'new_item'           => 'New Example',
+			'all_items'          => 'All Examples',
+			'view_item'          => 'View Examples',
+			'search_items'       => 'Search Examples',
+			'not_found'          =>  'No Examples Found',
 			'not_found_in_trash' => 'No Examples Found in Trash', 
 			'parent_item_colon'  => '',
-			'menu_name' 			=> 'Examples'
+			'menu_name'          => 'Examples'
 		);
 		
 		$args = array(
-			'labels' 				=> $labels,
-			'public' 				=> true,
+			'labels'             => $labels,
+			'public'             => true,
 			'publicly_queryable' => true,
-			'show_ui' 				=> true, 
-			'show_in_menu' 		=> true, 
-			'query_var' 			=> true,
-			'rewrite' 				=> true,
-			'capability_type' 	=> 'post',
-			'has_archive' 			=> false, 
-			'hierarchical' 		=> false,
-			'menu_position' 		=> null,
-			'supports' 				=> array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields')
+			'show_ui'            => true, 
+			'show_in_menu'       => true, 
+			'query_var'          => true,
+			'rewrite'            => true,
+			'capability_type'    => 'post',
+			'has_archive'        => false, 
+			'hierarchical'       => false,
+			'menu_position'      => null,
+			'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt', 'custom-fields')
 		);
 		register_post_type( 'example', $args );
 		
