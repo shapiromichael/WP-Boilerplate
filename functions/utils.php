a5d0bf4ecc442d8c3e6c3d6f7381340764781a09
Elisha Shapiro <elishas@sinapsa.co.il>
2012-08-31 10:05:02 +0300

    added "Read More" link function hook to until.php

--- functions/utils.php
+++ functions/utils.php
@@ -1,4 +1,12 @@
 <?php
 	
 	
+/*	
+ *	Lets you customize the more link after the post excerpt
+ */
+function excerpt_more_link($more) {
+	global $post;
+	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> Read more &raquo; </a>';
+}
+add_filter('excerpt_more', 'excerpt_more_link');
 	
