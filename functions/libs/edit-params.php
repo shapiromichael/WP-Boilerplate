03c51863d58d5086e798c0de5a1cfeb2b4053237
Elisha Shapiro <elishas@sinapsa.co.il>
2012-09-25 15:19:18 +0200

    Created a params file

---
+++ functions/libs/edit-params.php
@@ -0,0 +1,10 @@
+<?php
+
+	$global_params = array(
+		'search_placeholder' => array( 'default' => '', 'title' => 'Search This Blog',         'description' => 'Watermark text that will appear in the search field', 'section' => 'Default' ),
+		'footer_copyrights'  => array( 'default' => '', 'title' => 'Copyrights in the Footer', 'description' => 'for example: All rights reserved for Sinapsa &copy;', 'section' => 'Default' ),
+		'before_author'      => array( 'default' => '', 'title' => 'Before Author:',           'description' => 'Will appear before the author title',                 'section' => 'Default' ),
+		'bofore_date'        => array( 'default' => '', 'title' => 'Before Date:',             'description' => 'Will appear before the date',                         'section' => 'General' ),
+		'before_categories'  => array( 'default' => '', 'title' => 'Before Categories',        'description' => 'Will appear before category links',                   'section' => 'General' ),
+		'before_tags'        => array( 'default' => '', 'title' => 'Before Tags',              'description' => 'Will appear before tag links',                        'section' => 'General' ),
+	);
