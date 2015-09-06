1caf93f55d836d62b7940c19b5eec53676ec3d91
Michael Shapiro <michaels@syncode.io>
2015-09-06 22:08:43 +0300

    Removing 3rd metabox

--- functions/libs/metabox/demo.php
+++ functions/libs/metabox/demo.php
@@ -153,28 +153,4 @@ if (is_admin()){
     'local_images'   => false,          // Use local or hosted images (meta box images for add/remove)
     'use_with_theme' => false          //change path if used with theme set to true, false for a plugin or anything else for a custom path(default false).
   );
-  
-  
-  /*
-   * Initiate your 3rd meta box
-   */
-  $my_meta3 =  new Metabox($config3);
-  //first field of the group has 'group' => 'start' and last field has 'group' => 'end'
-  
-  //text field
-  $my_meta3->addText($prefix.'text_field_id',array('name'=> 'My Text ','group' => 'start'));
-  //textarea field
-  $my_meta3->addTextarea($prefix.'textarea_field_id',array('name'=> 'My Textarea '));
-  //checkbox field
-  $my_meta3->addCheckbox($prefix.'checkbox_field_id',array('name'=> 'My Checkbox '));
-  //select field
-  $my_meta3->addSelect($prefix.'select_field_id',array('selectkey1'=>'Select Value1','selectkey2'=>'Select Value2'),array('name'=> 'My select ', 'std'=> array('selectkey2')));
-  //radio field
-  $my_meta3->addRadio($prefix.'radio_field_id',array('radiokey1'=>'Radio Value1','radiokey2'=>'Radio Value2'),array('name'=> 'My Radio Filed', 'std'=> array('radionkey2'),'group' => 'end'));
-
-  /*
-   * Don't Forget to Close up the meta box Declaration 
-   */
-  //Finish Meta Box Declaration 
-  $my_meta3->Finish();
 }
